113f98356d9526a3ab6c8bcbbfd0b1233263f249
Evelin-lozano <evelinlozano163gmail.com>
2022-10-26 11:05:39 -0500

    second commit

--- Ejercicio-9/operacion9.php
+++ Ejercicio-9/operacion9.php
@@ -3,10 +3,10 @@ $asignatura = $_POST['asignatura'];
 $nota1 = $_POST['nota1'];
 $nota2 = $_POST['nota2'];
 $nota3 = $_POST['nota3'];
-$notae1= $nota1*30;
-$notae2= $nota2*30;
-$notae3= $nota3*40;
-$cdefinitiva= $notae1+ $notae2+$notae3;
+$notae1= $nota1*0.30;
+$notae2= $nota2*0.30;
+$notae3= $nota3*0.40;
+$cdefinitiva= $notae1+$notae2+$notae3;
 
 if($cdefinitiva>=4.0){
     echo "Asignatura:"." ".$asignatura." "."Aprobado";    
